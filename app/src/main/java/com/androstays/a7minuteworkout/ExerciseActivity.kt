1e5cb299d7eb55f51bd665b3522bb3ea8fdfcb1d
Naveen Prasanth <naveenprasanth2@gmail.com>
2022-08-27 21:38:31 +0530

    TillSound

--- app/src/main/java/com/androstays/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/androstays/a7minuteworkout/ExerciseActivity.kt
@@ -1,21 +1,30 @@
 package com.androstays.a7minuteworkout
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.androstays.a7minuteworkout.databinding.ActivityExerciseBinding
+import java.nio.file.Paths
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     var binding: ActivityExerciseBinding? = null
     private var restTimer: CountDownTimer? = null
     private var exerciseTimer: CountDownTimer? = null
     private var restProgress: Int = 0
     private var exerciseProgress: Int = 0
-    private var exerciseList: ExerciseModel? = null
+    private var exerciseList: ArrayList<ExerciseModel>? = null
     private var currentExercisePosition: Int = -1
+    private var tts: TextToSpeech? = null
+    private var player: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,20 +40,42 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+        tts = TextToSpeech(this, this)
+        exerciseList = Constants.defaultExerciseList()
         setupRestView()
     }
 
     private fun setupRestView() {
-        if (restTimer != null) {
+
+        try {
+            val soundURI =
+                Uri.parse("android.resource://com.androstays.a7minuteworkout/" + R.raw.press_start)
+            player = MediaPlayer.create(applicationContext, soundURI)
+            player?.isLooping = false
+            player?.start()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        restTimer?.let {
             restTimer?.cancel()
             restProgress = 0
         }
-        binding?.flProgressBarForExercise?.visibility = View.INVISIBLE
+        binding?.flRestView?.visibility = View.VISIBLE
+        binding?.tvTitle?.visibility = View.VISIBLE
+        binding?.tvUpcomingLabel?.visibility = View.VISIBLE
+        binding?.tvExerciseTitle?.visibility = View.VISIBLE
+        binding?.tvExerciseName?.visibility = View.INVISIBLE
+        binding?.flExerciseView?.visibility = View.INVISIBLE
+        binding?.ivImage?.visibility = View.INVISIBLE
         setRestProgressBar()
     }
 
     private fun setRestProgressBar() {
         binding?.progressBar?.progress = restProgress
+        if (currentExercisePosition < exerciseList!!.size - 1) {
+            binding?.tvExerciseTitle?.text = exerciseList!![currentExercisePosition + 1].getName()
+        }
         restTimer = object : CountDownTimer(10000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 restProgress++
@@ -53,53 +84,95 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                setupExerciseView()
+                if (currentExercisePosition < exerciseList!!.size) {
+                    currentExercisePosition++
+                    setupExerciseView()
+                } else {
+                    Toast.makeText(this@ExerciseActivity, "its done", Toast.LENGTH_SHORT).show()
+                }
             }
 
         }.start()
     }
 
 
-    private fun setRestProgressBarForExercise() {
-        binding?.progressBarForExercise?.progress = exerciseProgress
-
-        Thread.sleep(1000)
-        exerciseTimer = object : CountDownTimer(30000, 1000) {
+    private fun setExerciseProgressBar() {
+        binding?.progressBarExercise?.progress = exerciseProgress
+        exerciseTimer = object : CountDownTimer(3000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 exerciseProgress++
-                binding?.progressBarForExercise?.progress = 30 - exerciseProgress
-                binding?.tvTimerForExercise?.text = (30 - exerciseProgress).toString()
+                binding?.progressBarExercise?.progress = 30 - exerciseProgress
+                binding?.tvTimerExercise?.text = (30 - exerciseProgress).toString()
             }
 
             override fun onFinish() {
-                Toast.makeText(
-                    this@ExerciseActivity,
-                    "timer is done for exercise",
-                    Toast.LENGTH_SHORT
-                ).show()
+                if (currentExercisePosition < exerciseList!!.size - 1) {
+                    setupRestView()
+                } else {
+                    Toast.makeText(this@ExerciseActivity, "everything is done", Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
 
         }.start()
     }
 
-    private fun setupExerciseView(){
-        restProgress = 0
-        binding?.flProgressBar?.visibility = View.INVISIBLE
-        binding?.tvTitle?.text = getString(R.string.exerciseName)
-        binding?.flProgressBarForExercise?.visibility = View.VISIBLE
-        if(exerciseTimer!=null){
+    private fun setupExerciseView() {
+        binding?.flRestView?.visibility = View.INVISIBLE
+        binding?.tvUpcomingLabel?.visibility = View.INVISIBLE
+        binding?.tvExerciseTitle?.visibility = View.INVISIBLE
+        binding?.tvTitle?.visibility = View.INVISIBLE
+        binding?.tvExerciseName?.visibility = View.VISIBLE
+        binding?.flExerciseView?.visibility = View.VISIBLE
+        binding?.ivImage?.visibility = View.VISIBLE
+
+        exerciseList?.let { exerciseList!![currentExercisePosition].getName() }
+            ?.let { speakOut(it) }
+
+        exerciseTimer?.let {
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
-        setRestProgressBarForExercise()
+        binding?.ivImage?.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        binding?.tvExerciseName?.text = exerciseList!![currentExercisePosition].getName()
+        setExerciseProgressBar()
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts!!.setLanguage(Locale.US)
+            if ((result == TextToSpeech.LANG_MISSING_DATA) || (result == TextToSpeech.LANG_COUNTRY_AVAILABLE)) {
+                Log.e("TTS", "The language specified is not supported")
+            }
+        } else {
+            Log.e("TTS", "Initialisation Failed")
+        }
+    }
+
+    private fun speakOut(text: String) {
+        tts?.let { tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "") }
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        if (restTimer != null) {
+        restTimer?.let {
             restTimer?.cancel()
             restProgress = 0
+        }
+
+        exerciseTimer?.let {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
+        tts?.let {
+            tts?.stop()
+            tts?.shutdown()
+        }
+
+        player?.let {
+            player?.stop()
         }
 
         binding = null
--- app/src/main/java/com/androstays/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/androstays/a7minuteworkout/MainActivity.kt
@@ -3,8 +3,6 @@ package com.androstays.a7minuteworkout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.widget.Toast
 import com.androstays.a7minuteworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
